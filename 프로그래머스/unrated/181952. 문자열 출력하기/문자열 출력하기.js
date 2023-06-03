a24aeea170605c418bdb52114770f9a5e4f7cc43
송유진 <106574819+IrisNamu@users.noreply.github.com>
2023-06-03 17:36:42 +0900

    문자열 출력하기.js

--- 프로그래머스/unrated/181952. 문자열 출력하기/문자열 출력하기.js
+++ 프로그래머스/unrated/181952. 문자열 출력하기/문자열 출력하기.js
@@ -1,29 +1,9 @@
-
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
 const readline = require('readline');
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
-});
+}).on('line', console.log)
 
-let input = [];
-
-rl.on('line', function (line) {
-    input = [line];
-}).on('close',function(){
-    str = input[0];
-    console.log(str)
-});
+//.on('line', console.log)는 
+// Node.js의 EventEmitter에서 제공하는 메서드인 on을 사용하여
+// 'line' 이벤트에 대한 이벤트 핸들러를 등록하는 코드입니다.
