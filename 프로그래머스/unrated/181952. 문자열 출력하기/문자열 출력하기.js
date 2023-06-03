6c69d7bfd3bfe376ae546173c5a4b4e1dd662083
송유진 <106574819+IrisNamu@users.noreply.github.com>
2023-06-03 17:35:54 +0900

    [unrated] Title: 문자열 출력하기, Time: 51.37 ms, Memory: 32.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181952. 문자열 출력하기/문자열 출력하기.js
@@ -0,0 +1,29 @@
+
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+    input = [line];
+}).on('close',function(){
+    str = input[0];
+    console.log(str)
+});
